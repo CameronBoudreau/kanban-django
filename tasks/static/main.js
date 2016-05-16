71d5f9d33200530ec17b1f5722e11105a2f71806
CameronB <cboudreau6238@gmail.com>
2016-05-16 15:32:55 -0400

    working deletes

--- tasks/static/main.js
+++ tasks/static/main.js
@@ -1,5 +1,3 @@
-jQuery(function($) {
-
     function getCookie(name) {
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
@@ -28,19 +26,63 @@ jQuery(function($) {
             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
             }
-        }
+        }sdfasf
     });
+
 
     var add_to_list = function(card) {
         console.log(card.status + card.title + ": " + card.description)
         var $li = $('<li>');
+        var $del = $('<button id=' + card.id + '>Delete</button>');
         $li.text(card.title + ": " + card.description);
         if (card.status == 'Urgent') {
-            $li.appendTo($('#urgent'))
+            $li.appendTo($('#urgent'));
+            $del.appendTo($('#urgent')).click( function() {
+		        var sibling = $(this).prev();
+
+                $.ajax({
+                    url: 'http://localhost:8000/api/tasks/' + card.id,
+                    type: 'DELETE',
+                    success: function() {
+        				sibling.slideUp(300,function() {
+        					sibling.remove();
+                            $('#' + card.id).remove();
+            			});
+            		}
+                });
+            })
         } else if (card.status == 'Complete') {
-            $li.appendTo($('#complete'))
+            $li.appendTo($('#complete'));
+            $del.appendTo($('#complete')).click( function() {
+		        var sibling = $(this).prev();
+
+                $.ajax({
+                    url: 'http://localhost:8000/api/tasks/' + card.id,
+                    type: 'DELETE',
+                    success: function() {
+        				sibling.slideUp(300,function() {
+        					sibling.remove();
+                            $('#' + card.id).remove();
+            			});
+            		}
+                });
+            })
         } else {
-            $li.appendTo($('#inProgress'))
+            $li.appendTo($('#inProgress'));
+            $del.appendTo($('#inProgress')).click( function() {
+		        var sibling = $(this).prev();
+
+                $.ajax({
+                    url: 'http://localhost:8000/api/tasks/' + card.id,
+                    type: 'DELETE',
+                    success: function() {
+        				sibling.slideUp(300,function() {
+        					sibling.remove();
+                            $('#' + card.id).remove();
+            			});
+            		}
+                });
+            })
         }
     }
 
@@ -49,6 +91,7 @@ jQuery(function($) {
         });
 
     var $card = $('#card');
+
     var $title = $('input[name="title"]');
     var $description = $('textarea[name="description"]');
     var $status = $('select[name="status"]');
@@ -58,14 +101,7 @@ jQuery(function($) {
         console.log('you submitted the form');
         data = {'title': $title.val(), 'description': $description.val(), 'status': $status.val()};
 
-        $.post('http://localhost:8000/api/tasks/', data, add_to_list)
+        $.post('http://localhost:8000/api/tasks/', data, add_to_list);
 
-        // $.ajax({
-        //     method: 'post',
-        //     url: 'http://localhost:8000/api/tasks/',
-        //     data: data,
-        //     success: add_to_list
-        // });
         return false;
     });
-})
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.views.generic.base import RedirectView
 from . import views
 
 
 urlpatterns = [
-    # url(r'^$', views.api_root),
+    url(r'^$', RedirectView.as_view(url='/main/', permanent=False),
+        name='redirect'),
     url(r'^main/$', views.MainPage.as_view()),
     url(r'^api/tasks/$', views.CardList.as_view()),
     url(r'^api/tasks/(?P<pk>[0-9]+)/$', views.CardDetail.as_view()),
